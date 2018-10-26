d90855a066646445bc57433f85dabfdfed07dfb5
Fégan PIERRE <fegan.pierre@gmail.com>
2018-10-26 11:06:07 +0200

    Fix the fix

--- src/App.vue
+++ src/App.vue
@@ -14,8 +14,6 @@
 import List from './List.vue'
 import Mash from './Mash.vue'
 
-const PORT = process.env.PORT || 4000
-
 export default {
   name: 'app',
   components: {
@@ -29,7 +27,7 @@ export default {
   }),
   methods: {
     getUrls(callback) {
-      const url = `https://localhost:${PORT}/cats`
+      const url = 'https://calm-citadel-31631.herokuapp.com/'
       const xhr = new XMLHttpRequest()
       xhr.onload = (result) => {
         const data = JSON.parse(result.target.response)
